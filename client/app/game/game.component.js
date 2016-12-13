e8615655145b3eda6ab3e0dd78b6a2ad0c5f2f8c
Mahesh Makani <mahesh.makani@netnatives.co.uk>
2016-12-13 10:08:28 +0000

    0.2.1 - fix panel heights on game single view

--- client/app/game/game.component.js
+++ client/app/game/game.component.js
@@ -20,7 +20,7 @@ export class GameComponent {
   $onInit () {
     this.$http.get('/api/games')
       .then(response => {
-        //console.log(response.data)
+        // console.log(response.data)
         this.sortGames(response.data)
       })
   }
@@ -38,19 +38,26 @@ export class GameComponent {
 
 export class GameSingleComponent {
   /* @ngInject */
-  constructor ($http, $stateParams, Util) {
+  constructor ($http, $stateParams, Util, $window, $timeout) {
     this.$http = $http
+    this.$window = $window
+    this.$timeout = $timeout
     this.$stateParams = $stateParams
     this.Util = Util
   }
 
   $onInit () {
-    //console.log(this.$stateParams)
+    // console.log(this.$stateParams)
     this.$http.get(`/api/games/${this.$stateParams.id}`)
       .then(response => {
-        //console.log(response.data)
+        // console.log(response.data)
         this.game = response.data
         this.game.tournaments = _.reverse(_.sortBy(this.game.tournaments, o => new Date(o.date_time)))
+
+        this.$timeout(() => {
+          this.$window.$('.match-height').matchHeight()
+          this.$window.$('.match-height-1').matchHeight()
+        }, 250)
       })
   }
 
